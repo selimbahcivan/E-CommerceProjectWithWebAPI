035d4e3ae9fb4d3249aa9579a81e704a23d2a57c
YavuzSelim <y.selimbahcivan@gmail.com>
2022-05-15 20:10:14 +0300

    Add project files.

---
+++ WebAPIWithCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAPIWithCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
