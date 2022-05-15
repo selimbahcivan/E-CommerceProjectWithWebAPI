edd2f3362f077fb78638665cda7256a0a53fb72a
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-05-15 21:03:00 +0300

    Created User in Concrete folder.

---
+++ Entities/DTOs/UserDTOs/UserDetailDTO.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs.UserDTOs
+{
+    public class UserDetailDTO : IDTO
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
