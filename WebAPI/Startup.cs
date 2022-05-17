0a709c26edf08381d0aa4b8f74bcfe79d5bc58e6
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-05-17 12:33:31 +0300

    Startup configured

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,6 +1,11 @@
+using DataAccess.Abstract;
+using DataAccess.Concrete.Contexts;
+using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,8 +30,15 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            IServiceCollection servicesCollections = services.AddDbContext<ECommerceProjectWithWebAPIContext>(opt => opt.UseSqlServer("Data Source = \\.SQLEXPRESS;" +
+                " Initial Catalog = ECommerceProjectWithWebAPIDb; Integrated Security = True", options => options.MigrationsAssembly("DataAccess")
+                .MigrationsHistoryTable(HistoryRepository.DefaultTableName, "dbo")));
+
 
             services.AddControllers();
+            services.AddTransient<IUserDal, EfUserDal>();
+            //services.AddTransient<IUserService,UserService>()
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
