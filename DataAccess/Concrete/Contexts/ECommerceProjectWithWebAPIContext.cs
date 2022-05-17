fae24290f080558eb306bd3fc99cb0f34e367819
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-05-17 12:31:10 +0300

    Context added

---
+++ DataAccess/Concrete/Contexts/ECommerceProjectWithWebAPIContext.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.Concrete.EntityFramework.Mapping;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Contexts
+{
+    public class ECommerceProjectWithWebAPIContext : DbContext
+    {
+        public ECommerceProjectWithWebAPIContext(DbContextOptions<ECommerceProjectWithWebAPIContext> options) : base(options)
+        {
+
+        }
+        public ECommerceProjectWithWebAPIContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conString = "Server=.;Database=eCommerceProject;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(conString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+        
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
