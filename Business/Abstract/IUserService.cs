2299b1bdda857ccc0c05d5f443c4f08f3d9bc0b8
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-05-17 22:23:09 +0300

    Created IUserService in Business

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.DTOs.UserDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDetailDTO>> GetListAsync();
+        Task<UserDTO> GetByIdAsync(int id);
+        Task<UserDTO> AddAsync(UserAddDTO userAddDTO);
+        Task<UserUpdateDTO> UpdateAsync(UserUpdateDTO userUpdateDTO);
+        Task<bool> DeleteAsync(int id);
+    }
+}
