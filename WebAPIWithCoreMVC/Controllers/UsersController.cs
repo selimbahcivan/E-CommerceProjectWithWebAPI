53eaaeb7d9900136b9df0e9349bdc839d8a4950c
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-05-18 23:13:30 +0300

    Created UsersController

---
+++ WebAPIWithCoreMVC/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Entities.DTOs.UserDTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace WebAPIWithCoreMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        #region Defines
+        private readonly HttpClient _httpClient;
+        private string url = "http://localhost:63510/api/";
+        #endregion
+
+        #region Ctor
+        public UsersController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        } 
+        #endregion
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _httpClient.GetFromJsonAsync<List<UserDetailDTO>>(url + "Users/GetList");
+            return View(users);
+        }
+    }
+}
