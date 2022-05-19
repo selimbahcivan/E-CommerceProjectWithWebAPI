fcf2c4f4244d2b42bea4fa092a43c978ba792127
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-05-19 19:53:59 +0300

    Created UserDeleteViewModel

---
+++ WebAPIWithCoreMVC/ViewModels/UserDeleteViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIWithCoreMVC.ViewModels
+{
+    public class UserDeleteViewModel
+    {
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string GenderName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
+}
+
