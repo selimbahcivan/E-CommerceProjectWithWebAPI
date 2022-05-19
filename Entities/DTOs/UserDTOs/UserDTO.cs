a8024f740f92c47fbfb9b0f5a6be6e96a656000e
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-05-19 13:47:55 +0300

    Added Password prop to UserDTO class

--- Entities/DTOs/UserDTOs/UserDTO.cs
+++ Entities/DTOs/UserDTOs/UserDTO.cs
@@ -13,6 +13,7 @@ namespace Entities.DTOs.UserDTOs
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Password { get; set; }
         public bool Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
