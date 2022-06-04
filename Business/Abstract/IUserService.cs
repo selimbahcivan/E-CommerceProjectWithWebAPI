e288414fcefdd2be0402614dd2faddf66e9a3260
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-04 19:34:53 +0300

    GetListAsync and GetAsync Expression methods added for AutoMapping.

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,18 +1,27 @@
 ﻿using Core.Utilities.Responses;
+using Entities.Concrete;
 using Entities.DTOs.UserDTOs;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Business.Abstract
 {
     public interface IUserService
     {
-        Task<ApiDataResponse<IEnumerable<UserDetailDTO>>> GetListAsync();
-        Task<ApiDataResponse<UserDTO>> GetByIdAsync(int id);
-        Task<ApiDataResponse<UserDTO>> AddAsync(UserAddDTO userAddDTO);
-        Task<ApiDataResponse<UserUpdateDTO>> UpdateAsync(UserUpdateDTO userUpdateDTO);
-        Task<ApiDataResponse<bool>> DeleteAsync(int id);
-        //Task<ApiDataResponse<AccessToken>> Authenticate(UserForLoginDto userForLoginDto);
+        Task<ApiDataResponse<IEnumerable<UserDetailDTO>>> GetListAsync(Expression<Func<User, bool>> filter = null);
 
+        Task<ApiDataResponse<UserDTO>> GetAsync(Expression<Func<User, bool>> filter);
+
+        Task<ApiDataResponse<UserDTO>> GetByIdAsync(int id);
+
+        Task<ApiDataResponse<UserDTO>> AddAsync(UserAddDTO userAddDTO);
+
+        Task<ApiDataResponse<UserUpdateDTO>> UpdateAsync(UserUpdateDTO userUpdateDTO);
+
+        Task<ApiDataResponse<bool>> DeleteAsync(int id);
+
+        //Task<ApiDataResponse<AccessToken>> Authenticate(UserForLoginDto userForLoginDto);
     }
 }
