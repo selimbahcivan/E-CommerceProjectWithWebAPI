d04601cb5dff8d380eb0bdb81980f5fb1ac4ab90
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-04 19:37:01 +0300

    MappingProfile.cs added for AutoMapper

---
+++ Business/Mappings/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Entities.Concrete;
+using Entities.DTOs.UserDTOs;
+
+namespace Business.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDetailDTO>();
+            CreateMap<UserDetailDTO, User>();
+
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+
+            CreateMap<User, UserAddDTO>();
+            CreateMap<UserAddDTO, User>();
+
+            CreateMap<User, UserUpdateDTO>();
+            CreateMap<UserUpdateDTO, User>();
+        }
+    }
+}
