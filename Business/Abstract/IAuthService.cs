d53425f5affc09a17e81ceee0f2d5aac00989e3d
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-07 14:30:27 +0300

    IAuthService interface added

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Utilities.Responses;
+using Entities.DTOs.UserDTOs;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IAuthService
+    {
+        Task<ApiDataResponse<UserDTO>> LoginAsync(LoginDto loginDto);
+    }
+}
