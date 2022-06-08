05121001f91456519774c04baba6808b16ccfc67
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-08 14:39:46 +0300

    Added Controllers and Views to Admin Area

---
+++ WebAPIWithCoreMVC/Areas/Admin/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIWithCoreMVC.Areas.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
