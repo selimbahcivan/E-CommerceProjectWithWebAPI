06731f6cd565c95823c0b3459ba26429801995de
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-09 16:43:21 +0300

    Added IAuthApiService

---
+++ WebAPIWithCoreMVC/ApiServices/Interfaces/IAuthApiService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Utilities.Responses;
+using Entities.DTOs.Auth;
+using Entities.DTOs.User;
+using System.Threading.Tasks;
+
+namespace WebAPIWithCoreMVC.ApiServices.Interfaces
+{
+    public interface IAuthApiService
+    {
+        Task<ApiDataResponse<UserDTO>> LoginAsync(LoginDto loginDto);
+    }
+}
