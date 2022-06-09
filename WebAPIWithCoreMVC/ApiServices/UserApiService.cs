4c58319f91248c854dcb6a24fb6198547b7dc5fa
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-09 16:45:57 +0300

    IUserApiService added

---
+++ WebAPIWithCoreMVC/ApiServices/UserApiService.cs
@@ -0,0 +1,32 @@
+﻿using Core.Utilities.Responses;
+using Entities.DTOs.User;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WebAPIWithCoreMVC.ApiServices.Interfaces;
+
+namespace WebAPIWithCoreMVC.ApiServices
+{
+    public class UserApiService : IUserApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<UserDetailDTO>> GetListAsync()
+        {
+            var response = await _httpClient.GetAsync("Users/GetList");
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            var responseSuccess = await response.Content.ReadFromJsonAsync<ApiDataResponse<IEnumerable<UserDetailDTO>>>();
+            return responseSuccess.Data.ToList();
+        }
+    }
+}
