1532d56168b705c43a51aab392eb772634d2d001
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-09 16:43:43 +0300

    Added IUserApiService

---
+++ WebAPIWithCoreMVC/ApiServices/Interfaces/IUserApiService.cs
@@ -0,0 +1,11 @@
+﻿using Entities.DTOs.User;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebAPIWithCoreMVC.ApiServices.Interfaces
+{
+    public interface IUserApiService
+    {
+        Task<List<UserDetailDTO>> GetListAsync(); 
+    }
+}
