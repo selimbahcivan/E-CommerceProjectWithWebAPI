0edded4c2fde31b489d72b30327c2f84eef68fdc
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-06-11 22:13:11 +0300

    Created IMD5Service.cs in MD5 fd.

---
+++ Core/Utilities/Security/MD5/IMD5Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.MD5
+{
+    public interface IMD5Service
+    {
+        /// <summary>
+        /// Metni MD5'e dönüştür
+        /// </summary>
+        /// <param name="Text">string text</param>
+        /// <returns>string text</returns>
+        string ConvertTextToMD5(string Text);
+        /// <summary>
+        /// MD5'le şifreleme
+        /// </summary>
+        /// <param name="Text">string text</param>
+        /// <returns>string text</returns>
+        string Encrypt(string Text);
+        /// <summary>
+        /// Şifresini çöz (MD5)
+        /// </summary>
+        /// <param name="encryptedValue">string MD5</param>
+        /// <returns>string text</returns>
+        string Decrypt(string encryptedValue);
+
+    }
+}
